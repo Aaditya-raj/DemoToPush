201a534e80b343f55275a0c7d31ce2a8b3e32b3c
aaditya.ip <aaditya.ip@wissen.com>
2022-08-04 13:09:00 +0530

    Added new code to new repo

--- clone.bat
+++ clone.bat
@@ -6,13 +6,13 @@ echo.
 set /p name=Enter your name  
 echo Hello %name%
 
-git clone https://wissencloudcoe@dev.azure.com/wissencloudcoe/AzureCloudCoE/_git/AzureCloudCoE -b development cd
+git clone https://wissencloudcoe@dev.azure.com/wissencloudcoe/AzureCloudCoE/_git/AzureCloudCoE -b development
 
 git add .
 
-git commit -m "Added code to new repo"
+git commit -m "Added new code to new repo"
 
-git remote add origin https://github.com/Aaditya-raj/demoAdd.git
+git remote add origin https://github.com/Aaditya-raj/demoToPush.git
 
 git push -u origin master
 
